a3a6d6a03e5b8aa216b08d35f4d60949e6b4b7d7
Vladimír Ulman <ulman@fi.muni.cz>
2021-11-09 10:16:04 +0100

    ADD: common ground dialog implementation for these params

---
+++ src/main/java/cz/it4i/ulman/transfers/graphexport/ui/AbstractGraphExportableDlg.java
@@ -0,0 +1,38 @@
+package cz.it4i.ulman.transfers.graphexport.ui;
+
+import cz.it4i.ulman.transfers.graphexport.GraphExportable;
+import org.scijava.plugin.Parameter;
+
+public abstract class AbstractGraphExportableDlg implements GraphExportableFetcher {
+	@Parameter(label = "grid stepping along x")
+	int xColumnWidth = 50;
+
+	@Parameter(label = "grid stepping along y")
+	int yLineStep = 100;
+
+	@Parameter(label = "the bending point along y")
+	int defaultBendingPointAbsoluteOffsetY = -80;
+
+	@Parameter
+	int defaultNodeWidth  = 30;
+
+	@Parameter
+	int defaultNodeHeight = 30;
+
+	@Parameter(description = "RGB value in the hexadecimal format; 00 is black, FF is white")
+	int defaultNodeColour = 0xCCCCCC;
+
+
+	GraphExportable worker;
+	//
+	@Override
+	public GraphExportable getUnderlyingGraphExportable() {
+		worker.set_xColumnWidth(this.xColumnWidth);
+		worker.set_yLineStep(this.yLineStep);
+		worker.set_defaultBendingPointAbsoluteOffsetY(this.defaultBendingPointAbsoluteOffsetY);
+		worker.set_defaultNodeWidth(this.defaultNodeWidth);
+		worker.set_defaultNodeHeight(this.defaultNodeHeight);
+		worker.set_defaultNodeColour(this.defaultNodeColour);
+		return worker;
+	}
+}
