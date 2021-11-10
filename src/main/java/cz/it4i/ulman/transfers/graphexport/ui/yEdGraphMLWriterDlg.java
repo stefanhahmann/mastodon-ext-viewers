61e5b2d49a4373e11f198ed1f80741ee5410786f
Vladimír Ulman <ulman@fi.muni.cz>
2021-11-10 08:50:40 +0100

    CHG: yEd dlg switched to using the common one

--- src/main/java/cz/it4i/ulman/transfers/graphexport/ui/yEdGraphMLWriterDlg.java
+++ src/main/java/cz/it4i/ulman/transfers/graphexport/ui/yEdGraphMLWriterDlg.java
@@ -1,32 +1,21 @@
 package cz.it4i.ulman.transfers.graphexport.ui;
 
-import cz.it4i.ulman.transfers.graphexport.GraphExportable;
 import cz.it4i.ulman.transfers.graphexport.yEdGraphMLWriter;
 import org.scijava.command.Command;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 import org.scijava.widget.FileWidget;
-
 import java.io.File;
 
 @Plugin(type = Command.class, name = "Export into yEd")
-public class yEdGraphMLWriterDlg implements Command, GraphExportableFetcher {
+public class yEdGraphMLWriterDlg extends AbstractGraphExportableDlg implements Command {
 	// ------ options and setup of this particular export mode ------
 	@Parameter(label = "Define .graphml file to save the lineage: ", style = FileWidget.SAVE_STYLE)
 	private File graphMLfile = new File("/tmp/mastodon.graphml");
-
-	// ------ the exporting workhorse ------
-	private yEdGraphMLWriter worker;
 
 	// ------ after all options are set, the workhorse is to be created here ------
 	@Override
 	public void run() {
 		worker = new yEdGraphMLWriter(graphMLfile.getPath());
-	}
-
-	// ------ common way to return the underlying workhorse ------
-	@Override
-	public GraphExportable getUnderlyingGraphExportable() {
-		return worker;
 	}
 }
