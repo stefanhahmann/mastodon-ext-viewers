f9bfcc54b0ecb575f0285ea493e4a39a64a15fa6
Vladimír Ulman <ulman@fi.muni.cz>
2023-03-03 12:18:08 +0100

    CHG,ADD2/2: BdvToBlenderPlugin class that asks for params and...
    ..then uses the original
    BdvToBlenderView.openUseAutoCleanBdvToBlenderView()

---
+++ src/main/java/cz/it4i/ulman/transfers/BdvToBlenderPlugin.java
@@ -0,0 +1,59 @@
+/*
+ * BSD 2-Clause License
+ *
+ * Copyright (c) 2023, Vladimír Ulman
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package cz.it4i.ulman.transfers;
+
+import org.mastodon.mamut.BdvToBlenderView;
+import org.mastodon.mamut.plugin.MamutPluginAppModel;
+import org.scijava.command.Command;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
+@Plugin( type = Command.class, name = "Export all spots as they are currently displayed in an opened BDV" )
+public class BdvToBlenderPlugin implements Command {
+	@Parameter(persist = false)
+	private MamutPluginAppModel pluginAppModel;
+
+	@Parameter
+	private String connectURL = "localhost:9083";
+
+	@Parameter(label = "Nickname of this experiment data:")
+	private String clientName = "E1";
+
+	@Parameter(label = "Spheres scale factor:", min = "0.01")
+	float objScale = 1.0f;
+
+	@Override
+	public void run() {
+		new BdvToBlenderView(pluginAppModel)
+				.setSpheresScalingFactor(objScale)
+				.openUseAutoCleanBdvToBlenderView(
+						connectURL,
+						clientName,
+						"synchronized BDV windows");
+	}
+}
