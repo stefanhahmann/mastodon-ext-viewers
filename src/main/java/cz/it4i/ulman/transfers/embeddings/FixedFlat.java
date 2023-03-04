b31dea4d59f9979fecc52122021b3bc42114b85b
Vladimír Ulman <ulman@fi.muni.cz>
2023-03-04 07:27:11 +0100

    ADD: probably okay and the wanted projection

--- src/main/java/cz/it4i/ulman/transfers/embeddings/FixedFlat.java
+++ src/main/java/cz/it4i/ulman/transfers/embeddings/FixedFlat.java
@@ -42,6 +42,7 @@ import org.mastodon.pool.PoolCollectionWrapper;
 import org.mastodon.ui.coloring.FixedColorGenerator;
 import org.mastodon.ui.coloring.GraphColorGenerator;
 import org.mastodon.ui.coloring.TagSetGraphColorGenerator;
+import org.scijava.ItemVisibility;
 import org.scijava.command.Command;
 import org.scijava.command.DynamicCommand;
 import org.scijava.log.LogService;
@@ -54,25 +55,39 @@ import java.util.Optional;
 
 import static cz.it4i.ulman.transfers.graphexport.Utils.createVector3d;
 
-@Plugin(type = Command.class, name = "Poles method: Parameters")
+@Plugin(type = Command.class, name = "Flat View Parameters")
 public class FixedFlat extends DynamicCommand {
 	@Parameter(label = "Label of north-pole spot:")
 	public String spotNorthPoleName;
 
-	@Parameter(label = "Label of south-pole spot):")
+	@Parameter(label = "Label of south-pole spot:")
 	public String spotSouthPoleName;
 
+	@Parameter(visibility = ItemVisibility.MESSAGE, required = false)
+	private final String sewInfoMsg1 = "Considering a look from the outside, having tails-to-head orientation parallel with";
+	@Parameter(visibility = ItemVisibility.MESSAGE, required = false)
+	private final String sewInfoMsg2 = "the south-to-north orientation, choose surface spot that'd be in the flat view centre.";
+	@Parameter(label = "Label of the centre spot:")
+	public String spotViewCentreName;
+
+	@Parameter(label = "South-north direction stretch factor:")
+	public float elevSpacing = 1.0f;
+	@Parameter(label = "Perpendicular direction stretch factor:")
+	public float azimuthSpacing = 1.0f;
+	@Parameter(label = "Radius used to draw the spheres:")
+	public float spheresRadius = 1.0f;
+
 	@Parameter
-	private String connectURL = "localhost:9083";
+	public String connectURL = "localhost:9083";
 
 	@Parameter(label = "Nickname of this experiment data:")
-	private String clientName = "E1";
+	public String clientName = "E1";
 
-	@Parameter(label = "Nickname of this displayed (sub)tree:")
-	private String dataName = "view1";
+	@Parameter(label = "Nickname of this flat view:")
+	public String dataName = "view1";
 
 	@Parameter(label = "Choose color scheme:", initializer = "readAvailColors", choices = {})
-	private String colorScheme = "All white";
+	public String colorScheme = "All white";
 
 	void readAvailColors() {
 		//there gotta be at least one choice... e.g. when there's not a single tagset available
@@ -105,20 +120,37 @@ public class FixedFlat extends DynamicCommand {
 			return;
 		}
 		final Vector3d posN = createVector3d(searchSpot.get());
-
+		//
 		searchSpot = vertices.stream().filter(s -> s.getLabel().equals(spotSouthPoleName)).findFirst();
 		if (!searchSpot.isPresent()) {
 			logService.error("Couldn't find (south pole) spot with label "+spotSouthPoleName);
 			return;
 		}
-		final Vector3d posS = createVector3d(searchSpot.get());
+		final Vector3d centre = createVector3d(searchSpot.get());
+		centre.add(posN).div(2.0);
+		//
+		final Vector3d upVec = new Vector3d(posN).sub(centre).normalize();
 
-		final Vector3d centre = new Vector3d(posN).add(posS).div(2.0);
-		final Vector3d runner = new Vector3d(centre);
-		double distN = runner.sub(posN).length();
-		double distS = runner.set(centre).sub(posS).length();
-		final double distZ = Math.max(distS,distN);
-
+		searchSpot = vertices.stream().filter(s -> s.getLabel().equals(spotViewCentreName)).findFirst();
+		if (!searchSpot.isPresent()) {
+			logService.error("Couldn't find (view centre pole) spot with label "+ spotViewCentreName);
+			return;
+		}
+		final Vector3d frontVec = createVector3d(searchSpot.get());
+		frontVec.sub(centre);
+		//
+		//move along upVec-given orientation to find latCentre such that
+		//the angle originalNorthPole-latCentre-theFrontPole is perpendicular,
+		//that said, we subtract the upVec contribution from the frontVec
+		final Vector3d latCentre = new Vector3d(upVec);
+		latCentre.mul( upVec.dot(frontVec) ).add(centre);
+		frontVec.add(centre)    //moves back to where the "view centre pole" is
+				.sub(latCentre);  //a vector again; finally, from latCentre to the "view centre pole"
+				.normalize();
+		//
+		//upVec is a normal vector to a plane that includes latCentre and frontVec (and "view centre pole"),
+		//this creates the 3rd "coordinate axis" -- used to define fully the azimuth angle
+		final Vector3d sideVec = frontVec.cross(upVec, new Vector3d());
 
 		//<colors>
 		Optional<TagSetStructure.TagSet> ts = pluginAppModel.getAppModel().getModel()
@@ -135,6 +167,8 @@ public class FixedFlat extends DynamicCommand {
 		//</colors>
 
 		//sweeping
+		final Vector3d runner = new Vector3d();
+		final Vector3d runnerProjectedToLateralPlane = new Vector3d();
 		final SpotsIterator visitor = new SpotsIterator(pluginAppModel.getAppModel(),
 				logService.subLogger("flat export"));
 
@@ -152,6 +186,9 @@ public class FixedFlat extends DynamicCommand {
 			final BucketsWithGraphics.SphereParameters.Builder sBuilder
 					= BucketsWithGraphics.SphereParameters.newBuilder();
 
+			//send debug data
+			//end of: send debug data
+
 			visitor.visitRootsFromEntireGraph( root -> {
 				final BucketsWithGraphics.BatchOfGraphics.Builder nodeBuilder = BucketsWithGraphics.BatchOfGraphics.newBuilder()
 						.setClientID(conn.clientIdObj)
@@ -161,14 +198,26 @@ public class FixedFlat extends DynamicCommand {
 
 				visitor.visitDownstreamSpots(root, spot -> {
 					runner.set(spot.getFloatPosition(0),spot.getFloatPosition(1),spot.getFloatPosition(2));
-					runner.sub(centre);
+					runnerProjectedToLateralPlane.set(runner); //a copy
+
+					runner.sub(centre).normalize();
+					double elevAngle = Math.acos( runner.dot(upVec) );
+
+					runnerProjectedToLateralPlane.sub(latCentre);
+					runner.set(upVec);
+					runner.mul( -1.0 * upVec.dot(runnerProjectedToLateralPlane) );
+					runnerProjectedToLateralPlane.add( runner );
+
+					double azimuthAng = Math.acos( runnerProjectedToLateralPlane.normalize().dot(frontVec) );
+					azimuthAng *= runnerProjectedToLateralPlane.dot(sideVec) < 0 ? +1 : -1;
+
 					sBuilder.setCentre( vBuilder
 							//updates the builder content and builds inside setCentre()
-							.setX(10.f*(float)Math.atan2(runner.y, runner.x))
-							.setY(10.f*(float)Math.asin(runner.z/distZ))
-							.setZ(0) );
+							.setX( azimuthSpacing*(float)azimuthAng )
+							.setY( elevSpacing*(float)elevAngle )
+							.setZ( 0 ) );
 					sBuilder.setTime(spot.getTimepoint());
-					sBuilder.setRadius(0.5f);
+					sBuilder.setRadius(spheresRadius);
 					sBuilder.setColorXRGB( colorizer.color(spot) );
 					//logService.info("adding sphere at: "+sBuilder.getTime());
 					nodeBuilder.addSpheres(sBuilder);
